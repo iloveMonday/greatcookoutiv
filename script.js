159b9ad5a5f4f0dfd7c8541db1660a1e0f92280c
IloveMonday <iloveyouchandlerbing@gmail.com>
2025-07-22 20:00:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+function month(num){
+    if (num ==1){
+        return "Jan"
+    }
+    else if (num ==2){
+        return "Feb"
+    }
+    else if (num ==3){
+        return "Mar"
+    }
+    else if (num ==4){
+        return "Apr"
+    }
+    else if (num ==5){
+        return "May"
+    }
+    else if (num ==6){
+        return "Jun"
+    }
+    else if (num ==7){
+        return "Jul"
+    }
+    else if (num ==8){
+        return "Aug"
+    }
+    else if (num ==9){
+        return "Sep"
+    }
+    else if (num ==10){
+        return "Oct"
+    }
+    else if (num ==11){
+        return "RIVRI"
+    }
+    else if (num ==12){
+        return "Nov"
+    }
+    else return "Dec"
+    
+
+}
